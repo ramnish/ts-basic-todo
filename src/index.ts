2093ba0f358c4acef6a26d254a42aa6f8578dffc
ramnish <mr.ramnish@gmail.com>
2023-07-01 12:06:39 +0530

    simple ts to-do list using snowpack

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,56 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {v4 as uuidV4} from "uuid"
 
-import confetti from 'canvas-confetti';
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+type Task = {
+ id: string,
+ title: string,
+ completed: boolean,
+ createdAt: Date, 
+}
+
+const tasks : Task[] = loadTasks()
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.querySelector<HTMLFormElement>("#new-task-form")
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+
+tasks.forEach(addListItem);
+
+form?.addEventListener("submit",  e => {
+  e.preventDefault()
+  if (input?.value == "" || input?.value == null) return
+  const task : Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+  tasks.push(task)
+  addListItem(task)
+  saveTasks();
+  input.value = ""
+});
+
+function addListItem(task : Task) {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
+  checkbox.addEventListener("change" , e => {
+    task.completed = checkbox.checked    
+    console.log(tasks)
+  })
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+}
+
+function loadTasks() : Task[] {
+  const taskJSON = localStorage.getItem("tasks");
+  if (taskJSON == null) return []
+  return JSON.parse(taskJSON);
+}
+
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
